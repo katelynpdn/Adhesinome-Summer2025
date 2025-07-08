907bba89ad0bbb208449c5a901afac78c1ae6573
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-07-08 17:47:20 -0500

    Pipeline changes

--- 01-three-part-adhesin-test/adhesinPipeline.sh
+++ 01-three-part-adhesin-test/adhesinPipeline.sh
@@ -45,7 +45,7 @@ tail -n +4 fungalrv_output > tmpfile && mv tmpfile fungalrv_output
 grep -v '^#' signalP_output > tmpfile && mv tmpfile signalP_output
 # Combine all output into table
 python ../scripts/parse_all_output.py fungalrv_output predgpi_output signalP_output proteinTable
-# Extract sequences satisfying all 3 conditions
-python extractSeq.py proteinTable.csv "$proteomeFile" > finalList.fasta
+# Extract sequences satisfying all 3 conditions and satisfying at least 1 condition into ../results/seqList_all and ../results/seqList_other
+python extractSeq.py proteinTable.csv "$proteomeFile"
 
 echo "Results saved to results/$outputDirectory"
--- runPipeline.sh
+++ runPipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # title: Run Entire Pipeline: Part 1 (FungalRV, SignalP, PredGPI) and Part 2 (Annotation) on given list of panproteomes
 # author: Katelyn Nguyen
-# date: 2025-06-23
+# date: 2025-07-08
 
 set -e
 
@@ -17,19 +17,30 @@ outDir=$3
 proteomeList=()
 while read -r line; do
     proteomeList+=("$line")
-done < $inputFile
+done < "$inputFile"
 
-for proteome in $proteomeList
+# Save directory of script
+baseDir="$(cd "$(dirname "$0")" && pwd)"
+for proteome in "${proteomeList[@]}"
 do
-    cd data
+    # Download proteome
+    cd "$baseDir/data"
     wget "https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/pan_proteomes/$proteome"
     gunzip "$proteome"
 
+    # Run part 01
     cd ../01-three-part-adhesin-test
-    ./adhesinPipeline.sh "${proteome}.fasta" "$outDir"
+    ./adhesinPipeline.sh "${baseDir}/data/${proteome}.fasta" "$outDir"
     echo "Proteome $proteome Part 01 complete, check 01-three-part-adhesin-test/results."
 
+    # Run part 02 on seqList_other.fasta (sequences satisfying at least 1 condition)
+    if [ ! -f results/seqList_other.fasta ]; then
+        echo "Error: seqList_other.fasta not found for $proteome"
+        exit 1
+    fi
+    cp results/seqList_other.fasta ../02-adhesin-annotate/data
     cd ../02-adhesin-annotate/scripts
-    ./02-pipeline.sh "${proteome}.fasta" "$2" "../01-three-part-adhesin-test/results/$outDir" "$outDir"
-    echo "Proteome $proteome Part 02 complete, check 02-three-part-adhesin-test/results."
+    ./02-pipeline.sh "../data/seqList_other.fasta" "$2" "../01-three-part-adhesin-test/results/$outDir" "$outDir"
+    echo "Proteome $proteome Part 02 complete, check 02-adhesin-annotate/results."
 done
+cd "$baseDir"
